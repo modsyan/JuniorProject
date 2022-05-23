aab410f7fa20f62bf8f5b64f820141ecb26d8322
modsyan <modclover7@gmail.com>
2022-05-23 11:20:39 +0200

    massage

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println();
-    }
 
-    Person p1 = new Person();
-}
+
+        //// msg from me 
+        
+    }}
