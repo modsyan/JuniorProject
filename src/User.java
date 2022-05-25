987bac7019d4917d0f6b47bb096c5b101716ab71
ahmed5202k <82845706+ahmed5202k@users.noreply.github.com>
2022-05-25 20:11:28 +0200

    last update

--- src/User.java
+++ src/User.java
@@ -1,7 +1,27 @@
 public class User extends Person {
     int Age;
-    double Wength;
+    double hight;
     double Weidth;
+
+    public User(){
+        this.Age = null;
+        this.hight = null;
+        this.Weidth = null;
+    }
+
+    public User(
+        int Age;
+        double hight;
+        double Weidth;
+
+
+    ){
+        this.Age = Age;
+        this.hight = hight;
+        this.Weidth = Weidth;
+
+    }
+
     
 
 }
